05bd4e97fa0b23a7e022721ec84905ad7c89b81b
Abhishek Gupta <142497451+abhishekgupta0803@users.noreply.github.com>
2025-05-20 16:50:44 +0530

    Update data.js

--- init/data.js
+++ init/data.js
@@ -1,7 +1,8 @@
 const sampleListings = [
   {
     title: "Cozy Beachfront Cottage",
-    description:
+    description: 
+      
       "Escape to this charming beachfront cottage for a relaxing getaway. Enjoy stunning ocean views and easy access to the beach.",
     image: {
       filename: "listingimage",
